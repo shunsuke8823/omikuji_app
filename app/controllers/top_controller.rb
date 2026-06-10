9a637d2b2f07cff97d970563601f9f48ec9dfe9a
sakuramairi <shunsuke.w0416@gmail.com>
2026-06-10 15:21:22 +0900

    おみくじ機能実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,10 @@
 class TopController < ApplicationController
   def index
-    
+    # トップページ(ボタン表示のみ)
   end
-end
+
+  def omikuji
+    results = ['大吉', '中吉', '小吉', '吉', '末吉']
+    @result = results.sample
+  end
+  end
